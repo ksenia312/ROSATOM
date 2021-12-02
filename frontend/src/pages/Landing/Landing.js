a9646e10ccddfdb24ccf3a65e2edd935ba21f1e0
ksenia312 <ksynikitina.02@mail.ru>
2021-12-02 19:48:10 +0300

    шрифт

--- frontend/src/pages/Landing/Landing.js
+++ frontend/src/pages/Landing/Landing.js
@@ -3,7 +3,6 @@ import {connect} from "react-redux";
 import {Header} from "@components";
 import style from './Landing.module.scss'
 import {push} from "connected-react-router";
-import Button from "@ui/Button";
 
 export default connect(
   (state) => ({}),
@@ -18,7 +17,14 @@ export default connect(
       <div className={style.landing__container}>
         <Header/>
         <div className={style.content__container}>
-
+          <div>Ежегодно в России происходит большое количество чрезвычайных ситуаций, приводящих к экологическому
+            ущербу, в том числе, нефтеразливы.
+            Согласно данным Министерства энергетики только за 2019 год на предприятиях топливно-энергетического
+            комплекса произошло более 17 тысяч аварий с разливами нефти. Из них 10,5 тысячи случаев случились на
+            нефтепроводах.
+            Мы обнаруживаем факты аварий на нефтепроводах, оцениваем потенциальный ущерб природе, а также продвигаем в
+            массы необходимость помощи пострадавшим животным.
+          </div>
 
         </div>
       </div>

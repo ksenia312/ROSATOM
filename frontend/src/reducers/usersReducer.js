95a05de2f66aa925ee0470c90441f6f024548416
ksenia312 <ksynikitina.02@mail.ru>
2021-12-03 10:26:05 +0300

    form

--- frontend/src/reducers/usersReducer.js
+++ frontend/src/reducers/usersReducer.js
@@ -2,8 +2,7 @@ import {fromJS} from "immutable"
 import {types} from "@reducers/common";
 
 export const usersInitialState = fromJS({
-  users_data: {},
-  access_token:''
+  users_data: {}
 })
 
 export const usersReducer = (state = {}, action) => {
@@ -13,7 +12,6 @@ export const usersReducer = (state = {}, action) => {
       return state
         .mergeDeepIn(['users_data'], fromJS(action))
     case types.USER__FETCH_FORM:
-      console.log(action)
       return state
         .set('access_token', fromJS(action.access_token))
     default:
